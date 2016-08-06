c8b9caa9ba84ced2d5a2a9395d0aaa635aefc88e
chanthicha <FaiFai@faifais-macbook-pro.local>
2016-08-07 01:28:37 +0700

    Initial Commit

---
+++ mapzoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapzoo
+//
+//  Created by FaiFai on 8/7/2559 BE.
+//  Copyright © 2559 FaiFai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
